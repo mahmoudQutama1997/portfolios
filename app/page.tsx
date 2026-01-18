568f57f01418320a29dc9f52e9be9421b1b748a1
mahmoudQutama1997 <mahmoudQutama1997@github,com>
2026-01-18 20:07:23 +0200

    deploy-ready

---
+++ app/page.tsx
@@ -0,0 +1,88 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <div className="header">
+        <h1>Eng.Mahmoud Qutama</h1>
+        <div className="list">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Projects</li>
+          <li>Contact Me</li>
+        </ul>
+        </div>
+        
+       
+      </div>
+      <div className="hero">
+      <p>Hi. I am Mahmoud Qutama, Full Stack Developer from Palestine ,28 years .Mechatronics engineer and software developer.interesting in buiding real web application</p>
+      <img src="/mahmoud.jpg" alt="Mahmoud" />
+</div>
+ <div className="hero-project">
+  <h2>My Projects as a Full stack Developer</h2>
+
+   <div className="carousel">
+    <button className="arrow left">‹</button>
+
+    <div className="carousel-track">
+
+      <div className="project-poster">
+        <img src="sand1.png" />
+        <div className="overlay">
+          <span className="rank">1</span>
+          <h3>Saned</h3>
+          <p className="project-email">mahmudqutami@gmail.com</p>
+
+        </div>
+      </div>
+
+      <div className="project-poster">
+        <img src="doctor.png" />
+        <div className="overlay">
+          <span className="rank">2</span>
+          <h3>Doctor Booking System</h3>
+          <p className="project-email">mahmudqutami@gmail.com</p>
+        </div>
+      </div>
+
+      <div className="project-poster">
+        <img src="doctor2.png" />
+        <div className="overlay">
+          <span className="rank">2</span>
+          <h3>Doctor Booking System</h3>
+          <p className="project-email">mahmudqutami@gmail.com</p>
+        </div>
+      </div>
+
+
+
+      <div className="project-poster">
+        <img src="doctor3.png" />
+        <div className="overlay">
+          <span className="rank">2</span>
+          <h3>Doctor Booking System</h3>
+          <p className="project-email">mahmudqutami@gmail.com</p>
+        </div>
+      </div>
+
+
+      <div className="project-poster">
+        <img src="park.png" />
+        <div className="overlay">
+          <span className="rank">3</span>
+          <h3>ParkRamallah</h3>
+           <p className="project-email">mahmudqutami@gmail.com</p>
+        </div>
+      </div>
+
+    </div>
+
+    <button className="arrow right">›</button>
+  </div>
+  
+</div>
+</div>
+  );
+}
